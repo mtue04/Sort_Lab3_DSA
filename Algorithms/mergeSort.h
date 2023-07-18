578efc1b29335dd9dc3509c37d276102758c415b
Lqkhanh22 <139688568+Lqkhanh22@users.noreply.github.com>
2023-07-18 14:16:21 +0700

    Add files via upload

--- Algorithms/mergeSort.h
+++ Algorithms/mergeSort.h
@@ -3,8 +3,6 @@
 
 #include "sort.h"
 
-#define MAX_SIZE 500000
-
 void mergeSort(int arr[], int first, int last)
 {
 	if (first < last){
@@ -13,6 +11,19 @@ void mergeSort(int arr[], int first, int last)
 		mergeSort(arr, mid + 1, last);
 		merge(arr, first, mid, last);
 	}
+}
+
+unsigned long long bubbleSort(int arr[], int n)
+{
+	unsigned long long cmp = 0;
+	if (++cmp && first < last){
+		int mid = (first + last) / 2;
+		mergeSort(arr, first, mid);
+		mergeSort(arr, mid + 1, last);
+		merge(arr, first, mid, last);
+	}
+	
+	return cmp;
 }
 
 void merge(int arr[], int first, int mid, int last){
@@ -33,6 +44,28 @@ void merge(int arr[], int first, int mid, int last){
 		
 	for (index = first; index <= last; ++index)
 		arr[index] = tempArr[index];
+}
+
+unsigned long long merge(int arr[], int first, int mid, int last){
+	int first1 = first, last1 = mid;
+	int first2 = mid + 1, last2 = last;
+	int tempArr[MAX_SIZE];
+	int index = first1;
+	while ((++cmp && first1 <= last1) && (++cmp && first2 <= last2)){
+		if (++cmp && arr[first1] <= arr[first2])
+			tempArr[index++] = arr[first1++];
+		else
+			tempArr[index++] = arr[first2++];
+	}
+	while (++cmp && first1 <= last1)
+		tempArr[index++] = arr[first1++];
+	while (++cmp && first2 <= last2)
+		tempArr[index++] = arr[first2++];
+		
+	for (index = first; ++cmp && index <= last; ++index)
+		arr[index] = tempArr[index];
+		
+	return cmp;
 }
 
 #endif
--- Algorithms/selectionSort.h
+++ Algorithms/selectionSort.h
@@ -14,4 +14,18 @@ void selectionSort(int arr[], int n){
 	}
 }
 
+unsigned long long selectionSort(int arr[], int n){
+	unsigned long long cmp = 0;
+	for (int i = 0; ++cmp && i < n - 1; ++i){
+		int minIdx = i;
+		
+		for (int j = i + 1; ++cmp && j < n; ++j)
+			if (++cmp && arr[j] < arr[minIdx])
+				minIdx = j;
+		swap(arr[minIdx], arr[i]);
+	}
+	
+	return cmp;
+}
+
 #endif
