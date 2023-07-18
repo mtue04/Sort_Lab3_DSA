80e255b43214f8e9c6f14123b6d3728059630cbd
Lqkhanh22 <139688568+Lqkhanh22@users.noreply.github.com>
2023-07-18 14:21:20 +0700

    Add files via upload

--- Algorithms/shellSort.h
+++ Algorithms/shellSort.h
@@ -16,4 +16,19 @@ void shellSort(int a[], int n){
     }
 }
 
+unsigned long long shellSort(int a[], int n){
+	unsigned long long cmp = 0;
+	int interval, i, j, temp;
+	for(interval = n/2; ++cmp && interval > 0; interval /= 2){
+		for(i = interval; ++cmp && i < n; i++){
+			temp = a[i];
+			for(j = i; cmp += 2 && j >= interval && a[j - interval] > temp; j -= interval){
+				a[j] = a[j - interval];				
+			}
+			a[j] = temp;
+		}
+    }
+    return cmp;
+}
+
 #endif
