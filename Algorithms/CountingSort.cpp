074bd77eb7260f970c37b05a2043c0dc084b5a97
hnamtrg <hoainamtrg2045@gmail.com>
2023-07-24 18:45:28 +0700

    Update CountingSort.cpp

--- Algorithms/CountingSort.cpp
+++ Algorithms/CountingSort.cpp
@@ -2,67 +2,75 @@
 #include "Lib.h"
 // -----   Counting Sort	-----//
 //  Algorithm
-void countingSortTime(int a[], int n)
-{
-    long int maxVal = a[0];
-    for (long int i = 1; i < n; i++)
-    {
-        if (a[i] > maxVal)
-        {
-            maxVal = a[i];
+void countingSort(int arr[], int n) {
+    //Find max value
+    long int maxVal = arr[0];
+    for (long int i = 1; i < n; i++) {
+        if (arr[i] > maxVal) {
+            maxVal = arr[i];
         }
     }
 
-    vector<unsigned long long> count(maxVal + 1, 0);
+    // perform distribution counting
+    int *f = new int[maxVal + 1]{0};
+    for (int i = 0; i < n; i++)
+        f[arr[i]]++;
 
-    for (long int i = 0; i < n; i++)
-    {
-        count[a[i]]++;
+    // accumulate sum of frequencies
+    for (int i = 1; i <= maxVal; i++)
+        f[i] = f[i - 1] + f[i];
+
+    // distribute values to their final positions
+    int *b = new int[n];
+    for (int i = n - 1; i >= 0; i--) {
+        b[f[arr[i]] - 1] = arr[i];
+        f[arr[i]]--;
     }
 
-    int index = 0;
-    for (long int i = 0; i <= maxVal; i++)
-    {
-        while (count[i] > 0)
-        {
-            a[index] = i;
-            index++;
-            count[i]--;
-        }
-    }
+    for (int i = 0; i < n; i++)
+        arr[i] = b[i];
+
+    // Free dynamically allocated memory
+    delete[] f;
+    delete[] b;
 }
 
 // Comparison Mode
-unsigned long long countingSortCompare(int a[], int n)
+unsigned long long countingSortCompare(int arr[], int n)
 {
     unsigned long long count_compare = 0;
 
-    long int maxVal = a[0];
+    long int maxVal = arr[0];
     for (long int i = 1; ++count_compare && i < n; i++)
     {
-        if (++count_compare && a[i] > maxVal)
+        if (++count_compare && arr[i] > maxVal)
         {
-            maxVal = a[i];
+            maxVal = arr[i];
         }
     }
 
-    vector<unsigned long long> count(maxVal + 1, 0);
+    // perform distribution counting
+    int *f = new int[maxVal + 1]{0};
+    for (int i = 0;++count_compare && i < n; i++)
+        f[arr[i]]++;
 
-    for (long int i = 0; ++count_compare && i < n; i++)
-    {
-        count[a[i]]++;
+    // accumulate sum of frequencies
+    for (int i = 1;++count_compare && i <= maxVal; i++)
+        f[i] = f[i - 1] + f[i];
+
+    // distribute values to their final positions
+    int *b = new int[n];
+    for (int i = n - 1;++count_compare && i >= 0; i--) {
+        b[f[arr[i]] - 1] = arr[i];
+        f[arr[i]]--;
     }
 
-    int index = 0;
-    for (long int i = 0; ++count_compare && i <= maxVal; i++)
-    {
-        while (++count_compare && count[i] > 0)
-        {
-            a[index] = i;
-            index++;
-            count[i]--;
-        }
-    }
+    for (int i = 0;++count_compare && i < n; i++)
+        arr[i] = b[i];
+
+    // Free dynamically allocated memory
+    delete[] f;
+    delete[] b;
 
     return count_compare;
 }
